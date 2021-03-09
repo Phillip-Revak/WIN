7bd246fdee7327ccb180ea7a2c63773b9c354d76
Phillip Revak <philliprevak@Phillips-MBP.lan1>
2021-03-09 14:32:43 -0500

    4.2Commit

--- Week4/GroupProject2/index.js
+++ Week4/GroupProject2/index.js
@@ -1,5 +1,5 @@
 
-
+// This is an if/else statement that receives the quality of service and determines the percentage of tip that you should give.
 function calculateTip(bills, quality, guests) {
     console.log(bills, quality, guests);
     if (quality === "Great") {
@@ -19,6 +19,7 @@ function calculateTip(bills, quality, guests) {
         tip = "Boi you must be outside your mind..."
         tipPer = "We outchea..";
         totalBill = "Hook me up with a refund!";
+        document.getElementById('harry').style.display='block'
         return tip, totalBill, tipPer;
     };
 
@@ -26,16 +27,20 @@ function calculateTip(bills, quality, guests) {
 };
 
 
-    
+// Then we select the calculate button.    
 let calculate = document.getElementById('calculate');
+
+// Then add an event listener to the calculate button. The event listener grabs the current value of the three input fields when it is clicked. The bill amount, the quality of service and the number of guests.
 calculate.addEventListener('click', function(event){
     var bill = Number(document.getElementById('billTotal').value);
     var serviceQuality = document.getElementById('quality').value;
     var numGuests = Number(document.getElementById('groupSize').value);
     console.log(bill, serviceQuality, numGuests);
 
+    // Inside the event listener we call the if/else function that we wrote at the top of the page with the new input values.
     calculateTip(bill, serviceQuality, numGuests);
 
+    // Then we select the three disabled input fields for tip amount, total bill with tip, and tip per person and insert a value into them for tip, totalbill, and tipPer that we returned from the calculateTip function.
     var tipAmount = document.querySelector('#tipAmount')
     var totalBillWithTip = document.querySelector('#totalBillWithTip')
     var tipPerson = document.querySelector('#tipPerson')
@@ -43,6 +48,7 @@ calculate.addEventListener('click', function(event){
     totalBillWithTip.value = totalBill;
     tipPerson.value = tipPer;
 
+    // Lastly we overwrite the CSS that is set to display:none to display: block so that the results are displayed.
     document.getElementById('results').style.display='block'
     
 });
