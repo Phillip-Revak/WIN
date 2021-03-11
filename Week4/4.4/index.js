84950bf865113f375c181ebe36878dc135fd0d22
Phillip Revak <philliprevak@Phillips-MBP.lan1>
2021-03-11 16:34:37 -0500

    4.4_Commit

---
+++ Week4/4.4/index.js
@@ -0,0 +1,40 @@
+// POSTMAN
+// Postman makes it easy to test, develop and document APIs by allowing users to quickly put together both simple and complex HTTP requests. Postman can be use as a chrome package app or add-on.
+
+// Authorization - verifies whether you have permission to access the data you want from the server.
+// When you send a request you often have to include parameters to ensure the request has permission to access and return the data you want.
+
+// If you don't specify authorization it will always be "Inherit auth from parent"
+// No Auth - anyone can use it.
+// Bearer Token - security token, any user with this can use it to access data resources.
+// Basic Auth - username and password
+// Digest Token - back and forth request for data, access, username/password, etc... (nonce and realm values).
+
+
+// GET, PUT, POST, PATCH, DELETE from each API (dog-ceo) (jsonplaceholder)
+// https://dog.ceo/dog-api/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Morikawa, Spieth
+// Rory(am), Xander(am), Hovland(am), Niemann, Reed
+// Conners(am), Griffin(am), Kirk
+// Ooust(am), Ancer, Tringale, Kiz(am), Burns(am), Palmer, Smith(am)
+// Nesmith, Steele, Werenski, Hatton(am)
+
+// Ownership
+// Cantlay, Finau, Xander, Morikawa, Rahm, Webb, Hovland, Niemann, Ancer, Zalatoris
+// Low
+// DJ, JT, Rory, DeChambeau, Reed
