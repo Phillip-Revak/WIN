6c7a37e6c6a456a7c76206816433103882a1c988
Phillip Revak <philliprevak@Phillips-MBP.lan1>
2021-03-17 16:45:24 -0400

    5.3Afternoon

---
+++ Week5/5.3/Intro_To_React/intro-proj/src/components/tutorial.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+class Tutorial extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="div-board">
+          <div className="row">
+          </div>
+        </div>
+        <div>
+          <button className="btn btn-success add-button">Add</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Tutorial;
+
+// Questions
+// Props
