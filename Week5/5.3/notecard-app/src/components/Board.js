aaa17a8710ab5199f25d1e7f50ccc2d6d955c84f
Phillip Revak <philliprevak@Phillips-MBP.lan1>
2021-03-18 15:38:23 -0400

    Tic-Tac-Toe_Commit

--- Week5/5.3/notecard-app/src/components/Board.js
+++ Week5/5.3/notecard-app/src/components/Board.js
@@ -5,6 +5,52 @@ import Note from './Note.js';
 class Board extends Component {
   constructor() {
     super();
+    this.state = {
+      notes: [
+        // {
+        //   title: "Class Notes",
+        //   body: "Always use constructors when extending another class"
+        // },
+        // {
+        //   title: "My New Address",
+        //   body: "2001 N Lonhill Phoenix, AZ 81234"
+        // },
+        // {
+        //   title: "React Notes",
+        //   body: "Everything in React is a component"
+        // }
+      ]
+    }
+  }
+
+  // Board.js Board Component Class Function
+
+  addNote() {
+    let notes = this.state.notes;
+    notes.push(
+      {
+        id: Date.now()
+      }
+    );
+    this.setState(
+      {
+        notes: this.state.notes
+      }
+    );
+  }
+
+  deleteNote(id){
+    let newNoteArr = this.state.notes;
+    newNoteArr.map((note, index) => {
+      if (id === note.id) {
+        newNoteArr.splice(index,1);
+      }
+    });
+    this.setState(
+      {
+        notes: newNoteArr
+      }
+    );
   }
 
   render() {
@@ -12,14 +58,21 @@ class Board extends Component {
       <div>
         <div className="div-board">
           <div className="row">
-          <Note title="Class Notes" body="Always use constructors when extending another class"/>
+
+          {
+            this.state.notes.map(item => {
+                return <Note title={item.title} body={item.body} key={item.id} id={item.id} deleteHandler={this.deleteNote.bind(this)}/>
+              })
+          }
+
+          {/* <Note title="Class Notes" body="Always use constructors when extending another class"/>
             <Note title="My New Address" body="2001 N Lonhill Phoenix, AZ 81234"/>
             <Note title="React Notes" body="Everything in React is a component"/>
-            <Note />
+            <Note /> */}
           </div>
         </div>
         <div>
-          <button className="btn btn-success add-button">Add</button>
+          <button className="btn btn-success add-button" onClick={this.addNote.bind(this)}>Add</button>
         </div>
       </div>
     );
---
+++ Week5/5.4/index.js
@@ -0,0 +1,25 @@
+// REACT
+// State - Component owned property that can store data that is meant to change.
+// Hooks - Look into it.
+// Lifecycle - Mounting - Updating - Unmounting.
+// Render() runs every time.
+// shouldComponentUpdate() if false prevents re-renders.
+// componentDidUpdate() will not run first time.
+
+// In react apps you pass props from parents to children. Declare the 'prop' in the parent (component) and then set up a receiver in the child (this.props."prop").
+
+
+
+
+
+
+// Niemann
+// Scott
+// Kirk, Ghim, 
+// Furyk,
+// Wallace, Poulter, Clark
+// Percy, Malnati, Taylor, Hoag
+
+// High
+// Sungjae
+// Tringale, Kizzire, Gooch, Westwood, Henley, Lowry, Davis, ZJ, Steele, Jones,
