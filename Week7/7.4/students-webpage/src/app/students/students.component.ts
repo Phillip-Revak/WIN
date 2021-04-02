aee5be78146a0b1f24c806533520eb2290de4646
Phillip Revak <philliprevak@Phillips-MBP.lan1>
2021-04-02 16:50:08 -0400

    7.5lateCommit

--- Week7/7.4/students-webpage/src/app/students/students.component.ts
+++ Week7/7.4/students-webpage/src/app/students/students.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Student } from '../student';
-import { STUDENTS } from '../mock-students';
+import { StudentService } from '../student.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-students',
@@ -8,17 +9,24 @@ import { STUDENTS } from '../mock-students';
   styleUrls: ['./students.component.css']
 })
 export class StudentsComponent implements OnInit {
-students = STUDENTS;
+students?: Student[];
+
+getStudents(): void {
+  this.studentService.getStudents()
+      .subscribe(students => this.students = students);
+}
 
 selectedStudent?: Student;
 
-  constructor() { }
+  constructor(private studentService: StudentService, private messageService: MessageService) { }
 
   ngOnInit(): void {
+    this.getStudents();
   }
-  
+
   onSelect(student: Student): void {
     this.selectedStudent = student;
+    this.messageService.add(`StudentComponent: Selected student id=${student.id}`);
   }
 
 }
