d1370a6931530af9a1a5642e8b712951539ecb8e
philliprevak <phillip.revak@gmail.com>
2021-05-05 08:15:11 -0400

    12.2Commit

--- Week12/src/com/tts/BadThreads.java
+++ Week12/src/com/tts/BadThreads.java
@@ -30,3 +30,10 @@ public class BadThreads {
 // 2. It will always say "Mares do eat oats" because the sleep time is shorter than for the alternative thread.
 // 3. If you change the sleep times so that they are reversed it prints out "Mares do not eat oats" as you would expect.
 // 4. Include another system.out.println before the main thread sleeps for 2000.
+
+
+
+// 1. The purpose of a case diagram is to outline a program before you start coding so that you have a good idea of where you are going and what code is responsible to other code. So that you don't get everything all mixed up and ruin your code.
+// 2. Object oriented programing and Unified Modeling Language are related as they think through the way code is layed out in a virtual map. Both help establish the way the code is organized.
+// 3. A component diagram does not represent the dynamic view of a system.
+// 4. Major UML building blocks are Things, Relationships, and Diagrams.
