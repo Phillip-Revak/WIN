c0dc759db66568955d760bdbe87bc655976fa7c5
philliprevak <phillip.revak@gmail.com>
2021-05-14 16:57:36 -0400

    13.5Commit

---
+++ Week13/week13day5/src/main/java/com/tts/week13day5/controller/SubscriberController.java
@@ -0,0 +1,50 @@
+package com.tts.week13day5.controller;
+
+import com.tts.week13day5.model.Subscriber;
+import com.tts.week13day5.repository.SubscriberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@Controller
+public class SubscriberController {
+
+    @Autowired
+    private SubscriberRepository subscriberRepository;
+
+    @GetMapping("/")
+    public String index(Subscriber subscriber) {
+        return "subscriber/index";
+    }
+
+    private Subscriber subscriber;
+
+    @PostMapping("/")
+    public String addNewSubscriber(Subscriber subscriber, Model model){
+        subscriberRepository.save(new Subscriber(
+                subscriber.getFirstName(),
+                subscriber.getLastName(),
+                subscriber.getUserName(),
+                subscriber.getSignedUp()
+        ));
+        model.addAttribute("firstName", subscriber.getFirstName());
+        model.addAttribute("lastName", subscriber.getLastName());
+        model.addAttribute("userName", subscriber.getUserName());
+        return "subscriber/result";
+    }
+
+    @GetMapping(value="/showSubscribers")
+    public String getSubscribers(Model model, String keyword) {
+        List<Subscriber> subscriberList = (List<Subscriber>) subscriberRepository.findAll();
+
+        model.addAttribute("subscribers", subscriberList);
+
+        return"subscriber/AllSubscribers";
+    }
+
+}
---
+++ Week13/week13day5/src/main/java/com/tts/week13day5/service/SubscriberService.java
@@ -0,0 +1,10 @@
+package com.tts.week13day5.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public abstract class SubscriberService {
+
+
+
+}
